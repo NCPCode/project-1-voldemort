43203fe47cb2b1faf863a147a4074f958afbeefd
Student <CSLAB313-1740@instr.cps.k12.il.us>
2017-11-08 15:45:32 -0600

    added player class

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pygame
 window = pygame.display.set_mode((300, 300))
 clock = pygame.time.Clock()
 
+class Player:
+	def __init__(self, color, coords, size, speed):
+		self.color = color
+		self.initial_coords = list(coords)
+		self.coords = coords
+		self.size = size
+		self.speed = speed
+
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
